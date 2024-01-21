e3a62b6595f425172416e79a97c8cbe9cb82b581
Devanshu Sharma <105267822+devanshusharma829@users.noreply.github.com>
2024-01-21 20:18:25 +0530

    Add files via upload

---
+++ Nth Fibonacci Number II.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  // step1:
+  vector<int> dp(n + 1);
+  // step2:
+  dp[1] = 1;
+  dp[0] = 0;
+  // step3
+  for (int i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  cout << dp[n] << endl;
+}
